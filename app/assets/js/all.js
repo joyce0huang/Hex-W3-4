39a1353f4a5e21bc4c41a06860b651dd48651192
joyce <joyce0723huang@gmail.com>
2022-08-06 15:29:25 +0800

    second commit

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,17 +1,17 @@
 // 漢堡條點擊
-var burger = document.querySelector('.burger')
-var burgerLine = document.querySelectorAll('.burger-line')
-var headerNav = document.querySelector('.header-nav')
-var headerH1 = document.querySelector('.header-h1')
-burger.onclick = function () {
-  headerNav.classList.toggle('max-h-0')
-  headerNav.classList.toggle('max-h-24')
-  headerH1.classList.toggle('left-0')
-  headerH1.classList.toggle('left-1/2')
-  headerH1.classList.toggle('-translate-x-1/2')
-  burgerLine[0].classList.toggle('translate-y-2')
-  burgerLine[0].classList.toggle('rotate-45')
-  burgerLine[1].classList.toggle('rotate-45')
-  burgerLine[2].classList.toggle('-translate-y-2')
-  burgerLine[2].classList.toggle('-rotate-45')
-}
+// var burger = document.querySelector('.burger')
+// var burgerLine = document.querySelectorAll('.burger-line')
+// var headerNav = document.querySelector('.header-nav')
+// var headerH1 = document.querySelector('.header-h1')
+// burger.onclick = function () {
+//   headerNav.classList.toggle('max-h-0')
+//   headerNav.classList.toggle('max-h-24')
+//   headerH1.classList.toggle('left-0')
+//   headerH1.classList.toggle('left-1/2')
+//   headerH1.classList.toggle('-translate-x-1/2')
+//   burgerLine[0].classList.toggle('translate-y-2')
+//   burgerLine[0].classList.toggle('rotate-45')
+//   burgerLine[1].classList.toggle('rotate-45')
+//   burgerLine[2].classList.toggle('-translate-y-2')
+//   burgerLine[2].classList.toggle('-rotate-45')
+// }
