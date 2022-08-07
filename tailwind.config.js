aa7c9f8167bff6ca9daf92832aa0d46353076d17
joyce <joyce0723huang@gmail.com>
2022-08-08 00:26:48 +0800

    third commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,13 @@ module.exports = {
     container: {
       center: true, 
       padding: '12px',
+      screens: {
+        sm: '640px',
+        md: '720px',
+        lg: '1024px',
+        xl: '1280px',
+        '2xl': '1320px',
+      },
     },
     extend: {
       colors:{
