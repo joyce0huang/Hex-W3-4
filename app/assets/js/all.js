cb98bd180f6fd3015ed974a120e4d9572b699360
joyce <joyce0723huang@gmail.com>
2022-08-13 01:48:20 +0800

    sixth commit

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,21 +1,21 @@
-// 漢堡條點擊
-// var burger = document.querySelector('.burger')
-// var burgerLine = document.querySelectorAll('.burger-line')
-// var headerNav = document.querySelector('.header-nav')
-// var headerH1 = document.querySelector('.header-h1')
-// burger.onclick = function () {
-//   headerNav.classList.toggle('max-h-24')
-//   headerH1.classList.toggle('left-0')
-//   headerH1.classList.toggle('left-1/2')
-//   headerH1.classList.toggle('-translate-x-1/2')
-//   burgerLine[0].classList.toggle('translate-y-2')
-//   burgerLine[0].classList.toggle('rotate-45')
-//   burgerLine[1].classList.toggle('rotate-45')
-//   burgerLine[2].classList.toggle('-translate-y-2')
-//   burgerLine[2].classList.toggle('-rotate-45')
-// }
+// layout 漢堡條點擊
+var burger = document.querySelector('.burger')
+var burgerLine = document.querySelectorAll('.burger-line')
+var headerNav = document.querySelector('.header-nav')
+var headerH1 = document.querySelector('.header-h1')
+burger.onclick = function () {
+    headerNav.classList.toggle('max-h-24')
+    headerH1.classList.toggle('left-0')
+    headerH1.classList.toggle('left-1/2')
+    headerH1.classList.toggle('-translate-x-1/2')
+    burgerLine[0].classList.toggle('translate-y-2')
+    burgerLine[0].classList.toggle('rotate-45')
+    burgerLine[1].classList.toggle('rotate-45')
+    burgerLine[2].classList.toggle('-translate-y-2')
+    burgerLine[2].classList.toggle('-rotate-45')
+}
 
-
+// pages 各分頁
 // -----陣列
 // 部落格
 let blogListArr = [
@@ -93,7 +93,6 @@ let shopListArr = [
         shopTag: "高雄市"
     }
 ]
-
 
 // -----判斷
 // 部落格
@@ -235,4 +234,3 @@ else if(document.getElementById('product-list-sunglasses')){
         }
     }
 }
-
