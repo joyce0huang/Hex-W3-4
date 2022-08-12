40c39433ec53d2c35d96130d77fb574a9c598079
joyce <joyce0723huang@gmail.com>
2022-08-13 01:48:54 +0800

    Update 2022-08-12T17:48:52.509Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,23 +1,26 @@
 "use strict";
 
-// 漢堡條點擊
-// var burger = document.querySelector('.burger')
-// var burgerLine = document.querySelectorAll('.burger-line')
-// var headerNav = document.querySelector('.header-nav')
-// var headerH1 = document.querySelector('.header-h1')
-// burger.onclick = function () {
-//   headerNav.classList.toggle('max-h-24')
-//   headerH1.classList.toggle('left-0')
-//   headerH1.classList.toggle('left-1/2')
-//   headerH1.classList.toggle('-translate-x-1/2')
-//   burgerLine[0].classList.toggle('translate-y-2')
-//   burgerLine[0].classList.toggle('rotate-45')
-//   burgerLine[1].classList.toggle('rotate-45')
-//   burgerLine[2].classList.toggle('-translate-y-2')
-//   burgerLine[2].classList.toggle('-rotate-45')
-// }
+// layout 漢堡條點擊
+var burger = document.querySelector('.burger');
+var burgerLine = document.querySelectorAll('.burger-line');
+var headerNav = document.querySelector('.header-nav');
+var headerH1 = document.querySelector('.header-h1');
+
+burger.onclick = function () {
+  headerNav.classList.toggle('max-h-24');
+  headerH1.classList.toggle('left-0');
+  headerH1.classList.toggle('left-1/2');
+  headerH1.classList.toggle('-translate-x-1/2');
+  burgerLine[0].classList.toggle('translate-y-2');
+  burgerLine[0].classList.toggle('rotate-45');
+  burgerLine[1].classList.toggle('rotate-45');
+  burgerLine[2].classList.toggle('-translate-y-2');
+  burgerLine[2].classList.toggle('-rotate-45');
+}; // pages 各分頁
 // -----陣列
 // 部落格
+
+
 var blogListArr = [{
   blogTitle: "情人特別企劃",
   blogSub: "2020 Valentine’s Special",
